45b9b4ddd8b50649cb1ed3b27bced4be08a7f464
Shubhashish Verma <svashish305@gmail.com>
2020-10-12 19:18:56 +0530

    minor changes

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 function Dashboard(props) {
   return (
-    <div>
-      Welcome to Dashboard! <br />
-      You are {props.loggedInUser && ((props.loggedInUser.name && props.loggedInUser.name!== 'New Student') ? props.loggedInUser.name : props.loggedInUser.email.split('@')[0])}! <br /> 
-      Admin status : {props.isAdmin ? 'True' : 'False'}
-    </div>
+    <React.Fragment>
+      {props.isAdmin ? (
+      <div>
+        Welcome to Dashboard! <br />
+        You are {props.loggedInUser && ((props.loggedInUser.name && props.loggedInUser.name!== 'New Student') ? props.loggedInUser.name : props.loggedInUser.email.split('@')[0])}! <br /> 
+        Admin status : True
+      </div>
+    ) : (
+      <div>
+        Welcome to Dashboard! <br />
+        You are {props.loggedInUser && ((props.loggedInUser.name && props.loggedInUser.name!== 'New Student') ? props.loggedInUser.name : props.loggedInUser.email.split('@')[0])}! <br /> 
+        Admin status : False
+      </div>
+    )}  
+    </React.Fragment>
   )
 }
 
