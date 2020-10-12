ea36e4a5ac5e471d4d2fd106f55205ed40ca0889
Shubhashish Verma <svashish305@gmail.com>
2020-10-12 16:44:14 +0530

    fixed registration in backend

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard(props) {
   return (
     <div>
-      Welcome to dashboard! <br />
+      Welcome to Dashboard! <br />
       You are {props.loggedInUser && props.loggedInUser.email.split('@')[0]}! <br /> 
       Admin status : {props.isAdmin ? 'True' : 'False'}
     </div>
