3a50c7b3474caf59a870ab9e062aa8a69853ee75
Shubhashish Verma <svashish305@gmail.com>
2020-10-12 18:09:14 +0530

    added custom icon

--- src/api-service.js
+++ src/api-service.js
@@ -28,4 +28,24 @@ export class API {
       }
     }).then(resp => resp.json())
   }
+
+  static getAllGroups(token) {
+    return fetch(`${process.env.REACT_APP_API_URL}/api/groups/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    }).then(resp => resp.json())
+  }
+
+  static getGroupById(group_id, token) {
+    return fetch(`${process.env.REACT_APP_API_URL}/api/groups/${group_id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    }).then(resp => resp.json())
+  }
 }
