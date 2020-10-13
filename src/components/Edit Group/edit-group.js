462ce75ca3f301e51078034519a92e8a9656c99e
Shubhashish Verma <svashish305@gmail.com>
2020-10-14 01:50:32 +0530

    WIP Edit Group component

--- src/components/Edit Group/edit-group.js
+++ src/components/Edit Group/edit-group.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { API } from '../../api-service';
 import { useCookies } from 'react-cookie';
 import { useMediaQuery } from 'react-responsive';
-import {Button} from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './edit-group.scss';
 
 function EditGroup(props) {
@@ -15,7 +15,8 @@ function EditGroup(props) {
 
   const [groupToEdit, setGroupToEdit] = useState(null);
   const [usersInGroup, setUsersInGroup] = useState([]);
-  const [updatedName, setUpdatedName] = useState('')
+  const [editUserClicked, setEditUserClicked] = useState(false);
+  const [userIdToEdit, setUserIdToEdit] = useState(null);
 
 
   useEffect(() => {
@@ -53,20 +54,65 @@ function EditGroup(props) {
     }
   }
 
-  return (
-    <div>
-      {props.editClicked ? (
-        <div className='main-container'>
-          {groupToEdit ? <input className='group-name-input' defaultValue={groupToEdit.name} onClick={evt => evt.target.select()}
-          onKeyDown={evt => evt.key === 'Enter' && evt.preventDefault() && updateGroupName(evt.target.value)}
-          onBlur={(evt) => updateGroupName(evt.target.value)}
-           /> : null}
-          <label className='members-text'>Members :</label><br />
+  const editUser = (userId) => {
+    setEditUserClicked(true)
+    setUserIdToEdit(userId)
+  }
 
-          <Button className='back-btn' onClick={() => setEditClicked(false)}>Go Back</Button>
+  const deleteUser = (userId) => {
+    API.deleteUser(userId, token['pg-token'])
+    .then(() => {
+      const remUsers = usersInGroup.filter(u => u.id !== userId)
+      setUsersInGroup(remUsers)
+    })
+    .catch(err => console.log(err))
+  }
+
+
+  return (
+    <React.Fragment>
+      {props.editClicked ? (
+        <div className='edit-group-container'>
+          {groupToEdit ? <input className='group-name-input float-left' defaultValue={groupToEdit.name} onClick={evt => evt.target.select()}
+          onKeyDown={evt => evt.key === 'Enter' && evt.preventDefault() && updateGroupName(evt.target.value)}
+          onBlur={(evt) => updateGroupName(evt.target.value)} /> : null}
+          <br />
+          {usersInGroup ? (<Container className='mt-20'>
+            <Row>
+              <label className='heading-text float-left'>Members :</label><br />
+            </Row>
+            <div className='heading-text float-left'>Name</div>
+            <div className='heading-text'>Availability</div>
+            {usersInGroup.length && usersInGroup.map((user) => {
+              return (
+                <div key={user && user.id}>
+                  <Row className='user-list-item mb-20'>
+                    <Col className='p-0 name-col'>
+                      <div className='user-name'>
+                        {(user.name && user.name !== 'New Student') ? user.name : (user.email && user.email.split('@')[0])}
+                      </div>
+                    </Col>
+                    <Col className='p-0'>
+                      <div className='user-availability'>{user.availability}</div>
+                    </Col>
+                    <div className='p-0 user-actions'>
+                      <Image className='edit-user-icon pointer' src='/assets/images/edit.svg' alt='edit' onClick={() => editUser(user.id)} fluid />
+                      <Image className='delete-user-icon pointer' src='/assets/images/delete.svg' alt='delete' onClick={() => deleteUser(user.id)} fluid />
+                    </div>
+                  </Row>
+                </div>
+              )
+            })}
+          </Container>) : null}
+          <div className='action-btns'>
+            <Button className='custom-sized-btn' onClick={() => setEditClicked(false)}>Go Back</Button>
+            <Button className='custom-sized-btn'>Schedule Meeting</Button>
+            <Button className='custom-sized-btn'>Add Student</Button>
+            <Button className='custom-sized-btn'>View Meetings</Button>
+          </div>
         </div>
       ) : null}
-    </div>
+    </React.Fragment>
   )
 }
 
