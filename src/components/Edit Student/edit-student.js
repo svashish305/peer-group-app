05cbc0c53f8f9c84eb35d3aaca58e2e6516e50ae
Shubhashish Verma <svashish305@gmail.com>
2020-10-14 19:44:49 +0530

    minor changes

--- src/components/Edit Student/edit-student.js
+++ src/components/Edit Student/edit-student.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { API } from '../../api-service';
+import { useCookies } from 'react-cookie';
+import { Dropdown, Button } from 'react-bootstrap';
 import './edit-student.scss';
 
 function EditStudent(props) {
+
+  const [token] = useCookies(['pg-token']);
+
+  const [userToEdit, setUserToEdit] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const userToEdit = await API.getUserById(props.userId, token['pg-token'])
+      .catch(err => console.log(err))
+      setUserToEdit(userToEdit)
+    }
+    fetchData()
+  }, 
+  // eslint-disable-next-line
+  [])
 
   function setEditUserClicked(value) {
     props.onEditUserClickedChange(value)
@@ -14,7 +31,7 @@ function EditStudent(props) {
 
   return (
     <div>
-      edit user called for {props.userId}
+      edit user called for {userToEdit && (userToEdit.name ? userToEdit.name : userToEdit.email.split('@')[0])}! <br/>
       <Button className='custom-sized-btn' onClick={() => setEditUserClicked(false)}>Go Back</Button>
 
     </div>
