42c39f151221c71c93091410245023940aac8bb1
Shubhashish Verma <svashish305@gmail.com>
2020-10-17 15:19:47 +0530

    admin flow complete

--- src/components/Meeting List/meeting-list.js
+++ src/components/Meeting List/meeting-list.js
@@ -1,20 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory, useLocation, useParams} from 'react-router-dom';
-import {Button} from 'react-bootstrap';
+import { API } from '../../api-service';
+import { useCookies } from 'react-cookie';
+import {Container, Row, Col, Button} from 'react-bootstrap';
+import { parseISO, format, addHours } from 'date-fns';
 import './meeting-list.scss';
 
 function MeetingList(props) {
 
-  const history = useHistory()
-  const location = useLocation()
+  const [token] = useCookies(['pg-token']);
 
-  const routeParams = useParams()
+  const [meetingsOfGroup, setMeetingsOfGroup] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const meetingsOfGroup = await API.getMeetingsOfGroup(props.group.id, token['pg-token'])
+      .catch(err => console.log(err))
+      setMeetingsOfGroup(meetingsOfGroup)
+    }
+    fetchData()
+  }, 
+  // eslint-disable-next-line
+  [])
+
+  function setShowMeetingsClicked(value) {
+    props.onShowMeetingsClickedChange(value)
+  }
 
   return (
-    <div>
-      This is meeting list component, route params caught: {routeParams.groupId} <br />
-      the current route is {location.pathname}
-      <Button onClick={() => history.goBack()}>Go Back</Button>
+    <div className='group-meeting-container'>
+      <Container className='p-0 d-flex flex-column'>
+        <label className='meeting-text'>{props.group.name} Meetings</label>
+        <Row className='mt-20'>
+          <Col>
+            <label className='float-left heading-text'>Date</label>
+          </Col>
+          <Col>
+            <label className='float-left heading-text'>Time</label>
+          </Col>
+        </Row>
+        {meetingsOfGroup && meetingsOfGroup.length && meetingsOfGroup.map(meeting => {
+          return (
+            <Row>
+              <Col className=''>
+                <div className='flex-center read-only'>{format(parseISO(meeting.time), "MMM d yyyy")}</div>
+              </Col>
+              <Col>
+                <div className='flex-center read-only'>{format(parseISO(meeting.time), "h:mm aa")} - {format(addHours(parseISO(meeting.time), 1), "h:mm aa")}</div>
+              </Col>
+            </Row>
+          )
+        })}
+      </Container>
+      <div className='d-flex'>
+        <footer>
+          <Button onClick={() => setShowMeetingsClicked(false)}>Go Back</Button>
+        </footer>
+      </div>
     </div>
   )
 }
