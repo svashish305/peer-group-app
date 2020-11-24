9ccdb72032e89b24a02bbd620ca8e71eff28b903
Shubhashish Verma <svashish305@gmail.com>
2020-11-25 04:09:37 +0530

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useFetch } from './hooks/useFetch';
 import Sidebar from './components/Sidebar/sidebar';
 import Dashboard from './components/Dashboard/dashboard';
 import './App.scss';
+import Spinner from 'react-bootstrap/Spinner';
 
 function App() {
 	// eslint-disable-next-line
@@ -14,14 +15,23 @@ function App() {
 		if (!token['pg-token']) window.location.href = '/';
 	}, [token]);
 
-	if (loading) return <h1>Loading...</h1>;
 	if (error) return <h1>Error loading dashboard</h1>;
 
 	return (
 		<div className='App'>
 			<header className='App-header'>Peer Group App</header>
-			<Sidebar loggedInUser={loggedInUser} />
-			<Dashboard loggedInUser={loggedInUser} isAdmin={isAdmin} />
+			{loading ? (
+				<div className='mt-66 flex-center'>
+					<Spinner animation='border' role='status' variant='primary'>
+						<span className='sr-only'>Loading...</span>
+					</Spinner>
+				</div>
+			) : (
+				<React.Fragment>
+					<Sidebar loggedInUser={loggedInUser} />
+					<Dashboard loggedInUser={loggedInUser} isAdmin={isAdmin} />
+				</React.Fragment>
+			)}
 		</div>
 	);
 }
